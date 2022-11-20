940bcb1b66ad4b9b67665b666549da56538c1ff8
caleb jephuneh <calebjephunneh3@gmail.com>
2022-11-20 17:55:53 +0300

    code

--- src/Row.jsx
+++ src/Row.jsx
@@ -1,9 +1,17 @@
-const Row = (title) =>{
+import React, { useState, useEffect } from 'react'
+import axios from 
+function Row({title}){
+    const[movies, setMovies] = useState([]);
+
+    useEffect(() => {
+
+    }, [])
+
+
     return(
         <div>
-            {/*title*/}
-            <h1>{title}</h1>
-
+          <h2>{title}</h2> 
+          {/* container -> posters */}  
         </div>
     )
 }
