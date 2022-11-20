18042bb92cf5330efc254090a04b7d2851455292
caleb jephuneh <calebjephunneh3@gmail.com>
2022-11-20 19:02:32 +0300

    more

--- src/Row.jsx
+++ src/Row.jsx
@@ -1,17 +1,28 @@
 import React, { useState, useEffect } from 'react'
-import axios from 
+import axios from './axios'
 function Row({title}){
     const[movies, setMovies] = useState([]);
 
     useEffect(() => {
+        async function fetchData(){
+            const request = await axios.get(fetchUrl)
+            console.log(request)
+            return request
+        }
+        fetchData() 
 
-    }, [])
+    }, [fetchUrl])
+
+    console.log(movies)
 
 
     return(
         <div>
           <h2>{title}</h2> 
           {/* container -> posters */}  
+          {movie.map(movie =>(
+        <img scr={movie.poster_path} alt={movie.name}
+          ))}
         </div>
     )
 }
